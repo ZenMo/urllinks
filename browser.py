a05a04ad73521a246e065258ac1eece11589eeef
ZenMo <Mastoris911@gmail.com>
2013-11-29 03:13:20 -0800

    working paragraph counting. redesigned counts. also added a cut helper for viewing html.

--- browser.py
+++ browser.py
@@ -2,9 +2,9 @@
 
 import urllib
 from bs4 import BeautifulSoup
-
-fhand1 = urllib.urlopen('http://www.google.com/')
-fhand2 = urllib.urlopen('http://www.google.com/')
+url = 'http://gizmodo.com/comet-ison-may-still-be-alive-1473478801'
+fhand1 = urllib.urlopen(url)
+fhand2 = urllib.urlopen(url)
 
 print fhand1,'\n'
 
@@ -13,14 +13,30 @@ for line in fhand1:
     line_count+=1
 
 rdata = fhand2.read()
-cut_data = rdata[:3000]
-
-soup = BeautifulSoup(fhand1)
 soup2 = BeautifulSoup(rdata)
-data = soup.prettify()
 
-print cut_data,'\n'
-print 'Charaters Shown: 3000\n'
-print 'Characters: ',len(rdata),'\n'
+cut = 0
+if cut==1:
+    cut_data = rdata[:3000]
+    print cut_data,'\n'
+elif cut==2:
+    print rdata
+else:
+    print
+    
+array = []
+char_count = 0
+
+print soup2.title.get_text(),'\n'
+for para in soup2.find_all("p"):
+    print para.get_text(),'\n'
+    array.append(para.get_text())
+for p in array:
+    for char in p:
+        char_count+=1
+#print soup2.get_text(),'\n'
+
+print '"p" tags: ', len(array),'\n'
 print 'Lines: ',line_count,'\n'
-print
+print 'Charater count: ', char_count,'\n'
+print 'All characters in HTML: ',len(rdata),'\n'
